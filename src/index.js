f2b5ad96fc8358d5e9e64ff61329dad3a3bd38a0
JohnDennehy <johndennehy101@gmail.com>
2021-08-27 15:14:44 +0100

    Basic scaffolding for portfolio.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import HomePage from './pages/homePage'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route component={HomePage} exact path='/' />
+        <Redirect from='*' to='/' />
+      </Switch>
+    </Router>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
---
+++ src/pages/homePage.js
@@ -0,0 +1,5 @@
+const HomePage = (props) => {
+  return <p>Testing</p>
+}
+
+export default HomePage
