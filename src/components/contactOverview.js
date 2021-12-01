2e4605ada8ef333b021151baead21d4b6d4d202d
JohnDennehy <johndennehy101@gmail.com>
2021-12-01 12:44:56 +0000

    Edited content on contact page. Edited portfolio images to align with naming convention for each project.

--- src/components/contactOverview.js
+++ src/components/contactOverview.js
@@ -7,10 +7,11 @@ const ContactOverview = () => {
       </div>
       <div id='mainContact'>
         <p>
-          I’d love to hear about what you’re working on and how I could help. I’m currently looking for a new role and am open to a wide
-          range of opportunities. My preference would be to find a position in a company in London. But I’m also happy to hear about
-          opportunites that don’t fit that description. I’m a hard-working and positive person who will always approach each task with a
-          sense of purpose and attention to detail. Please do feel free to check out my online profiles below and get in touch using the
+          I’m currently looking for a new role and am open to a wide range of opportunities. 
+          My preference would be to find a software development position with a hybrid working model in a company in Cork. 
+          But I’m also happy to hear about opportunites that don’t fit that description. 
+          I’m a hard-working and positive person who will always approach each task with a sense of purpose and attention to detail. 
+          Please do feel free to check out my online profiles below and get in touch using the
           form.
         </p>
         {/* <div id="icons"> */}
